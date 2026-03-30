ac809b3254394b3a36bd7e0e385f646cbd7ca6fe
liwenlu <2816842036@qq.com>
2026-03-30 08:47:52 +0800

    perf: 启用 Element Plus 按需导入

--- frontend-mobile/vite.config.js
+++ frontend-mobile/vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'css',
+          directives: true,
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'css',
+          directives: true,
+        }),
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
